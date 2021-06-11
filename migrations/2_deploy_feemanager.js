4f24f23fd84247dccbb88ffaa763b424d72a8fa9
zuohuahua <zuohuahua@elastos.org>
2021-06-11 16:05:19 +0800

    add Liquidation adapter

--- migrations/2_deploy_feemanager.js
+++ migrations/2_deploy_feemanager.js
@@ -3,7 +3,7 @@ const FeeManager = artifacts.require("FeeManager");
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(FeeManager,
         '0x', // _governance
-        '0x' // _daoPool
+        '0x73CB0A55Be009B30e63aD5830c85813414c66367' // _daoPool
         );
 
     console.log("***********************************************");
--- migrations/4_deploy_LiquiditySwap.js
+++ migrations/4_deploy_LiquiditySwap.js
@@ -1,15 +1,17 @@
 const LiquiditySwap = artifacts.require("LiquiditySwap");
+const FeeManager = artifacts.require("FeeManager");
 
 module.exports = async function (deployer, network, accounts) {
+    const feeManage = await FeeManager.deployed();
 
     await deployer.deploy(LiquiditySwap,
-        '0x', // falshloan address
+        '0x', // flashloan address
         '0x', // _governance
-        '0x', // _swapWrapper
-        '0x', // _weth
-        '0x', // _fETH
-        '0x', // _feeManager
-        '0x' // _comptroller
+        '0xCD83Fb2cb441127602e74860117c8E26E0864692', // _swapWrapper
+        '0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F', // _weth
+        '0x824151251B38056d54A15E56B73c54ba44811aF8', // _fETH
+        feeManage.address, // _feeManager
+        '0xb74633f2022452f377403B638167b0A135DB096d' // _comptroller
         );
 
     console.log("***********************************************");
